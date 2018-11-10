2a59fb95488cdeee390f822dd8c7788bc2218acf
Mirkan <mirkancaliskan@gmail.com>
2018-11-10 19:35:49 +0300

    Functionality added

--- script.js
+++ script.js
@@ -14,18 +14,65 @@ let uncheckedCount = 0;
 let todoId = 0;
 
 function newTodo() {
-  const todo = prompt('Create new TODO')
+  const todo = prompt("Enter new todo");
   if(todo === null) {
-    return
+    return;
   }
-  list.appendChild(li(todo))
 
-  todoCount++
+  list.appendChild(li(todo));
+  todoCount++;
   uncheckedCount++
-  updateCounters()
+  updateCounters();
 }
 
 function updateCounters() {
   itemCountSpan.innerText = todoCount;
-  
+  uncheckedCountSpan.innerText = uncheckedCount;
+}
+
+function li(todo) {
+  const li = document.createElement('li');
+  li.className = classNames.TODO_ITEM;
+  li.id = `todo-${todoId++}`
+  li.appendChild(checkbox());
+  li.appendChild(span(todo));
+  li.appendChild(deleteBtn(li.id));
+  return li;
+}
+
+function checkbox() {
+  const input = document.createElement(input);
+  input.setAttribute('type', 'checkbox');
+  input.onchange = function() {
+    if (input.checked) {
+      uncheckedCount--;
+    } else {
+      uncheckedCount++
+    }
+    updateCounters();
+    return true;
+  }
+  input.className = classNames.TODO_CHECKBOX;
+  return input;
+}
+
+function span(todo) {
+  const span = document.createElement('span');
+  span.className = classNames.TODO_TEXT;
+  span.innerText = todo;
+  return span;
+}
+
+function deleteBtn(id) {
+  const btn = document.createElement('button');
+  btn.className = classNames.TODO_DELETE;
+  btn.innerText = 'Delete';
+  btn.onClick = function() {
+    const li = getElementById(id);
+    todoCount--;
+    let checked = li.getElementsByTagName('input')[0].checked;
+    uncheckedCount = checked ? uncheckedCount : uncheckedCount - 1;
+    updateCounters();
+    li.remove()
+  }
 }
